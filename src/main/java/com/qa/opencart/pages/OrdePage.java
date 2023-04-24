74d5e2139aac0cd01e777cc756fff2184bfdeea4
Raji Thomas <rajithomas16@gmail.com>
2023-04-24 06:31:34 -0400

    updated order page

--- src/main/java/com/qa/opencart/pages/OrdePage.java
+++ src/main/java/com/qa/opencart/pages/OrdePage.java
@@ -1,9 +1,13 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class OrdePage {
 
 	
 	String order;
+	private By check=By .id("check");
+	
 	 public void order() {
 		 
 		 System.out.println("Order Page");
